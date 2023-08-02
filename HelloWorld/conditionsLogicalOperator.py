a39baf48476467fbfae3ccd306f97aaa4b14d31d
singhab23 <85208211+singhab23@users.noreply.github.com>
2023-08-02 09:04:19 -0500

    Add files via upload

--- HelloWorld/conditionsLogicalOperator.py
+++ HelloWorld/conditionsLogicalOperator.py
@@ -10,7 +10,7 @@ has_criminal_record = False
 if has_good_credit and high_income_level:
     print("Eligible for loan1")
 
-#using the "not" operator reverses the set value
+#using the "not" operator reverses the set value(Now both condition becomes true)
 
 if has_good_credit and not has_criminal_record:
     print("Eligible for loan2")
--- HelloWorld/exercise1.py
+++ HelloWorld/exercise1.py
@@ -3,9 +3,7 @@ metric = input("(L)bs or (K)g: ")
 
 if metric.upper() == "L" :
     print(f"You are {weight * 0.454} kgs")
-if metric.upper() == "K" :
+elif metric.upper() == "K" :
     print(f"You are {weight * 2.2} pounds")
 else:
-    print("Please enter correct metric value.")
-
-    #tried the git upload
+    print("Please enter a correct metric value.")
